c4963f45d350b3b9c8adde782c64dbc2df717de0
Abhishek Allamsetty <2019aallamse@tjhsst.edu>
2017-09-13 22:17:37 -0400

    Update text.js

--- js/text.js
+++ js/text.js
@@ -204,8 +204,5 @@ var text = {
 		return facts[random] + '\n';
 	},
 	
-	katex: function(){
-		return katex.render("c = \\pm\\sqrt{a^2 + b^2}", element);
-	}
 	contact: "say hey <a href='mailto:allamsettyabhishek@gmail.com'>here</a>\n"
 }
